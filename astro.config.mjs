421dff4a190f5656f0bd8a64ec888a5174725108
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2024-03-01 19:58:54 +0800

    Remove unnecessary code from previous test attempts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://interledger.org",
