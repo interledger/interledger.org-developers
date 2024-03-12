a6067fd1246b118829d7e2139401783472c29cb0
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2024-03-12 09:56:44 +0800

    feat: add excerpt/description to blog listing (#45)
    * Add excerpt to blog and bump dependencies
    
    * Remove experiment code
    
    * Adjust package.json

---
+++ src/utils/create-excerpt.js
@@ -0,0 +1,18 @@
+import MarkdownIt from "markdown-it";
+import { convert } from "html-to-text";
+const parser = new MarkdownIt();
+
+export const createExcerpt = (body) => {
+  const html = parser.render(body);
+  const options = {
+    wordwrap: null,
+    selectors: [
+      { selector: "a", options: { ignoreHref: true } },
+      { selector: "img", format: "skip" },
+      { selector: "figure", format: "skip" },
+    ],
+  };
+  const text = convert(html, options);
+  const distilled = convert(text, options);
+  return distilled;
+};
