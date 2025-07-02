ec29df5d155406e3dac18c155f631fa09eddadda
Anca Matei <ancamatei2012@gmail.com>
2025-07-02 16:11:50 +0300

    chore: add language switcher to navigation menu

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,19 +1,39 @@
-import { ui, defaultLang, showDefaultLang } from './ui';
+import { ui, defaultLang } from "./ui";
 
 export function getLangFromUrl(url: URL) {
-  const [, , lang] = url.pathname.split('/');
+  const [, , lang] = url.pathname.split("/");
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof typeof ui[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
-  }
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    const defaultStrings = ui[defaultLang];
+    const currentStrings = ui[lang] as Partial<typeof defaultStrings>;
+
+    return currentStrings[key] ?? defaultStrings[key];
+  };
 }
 
 export function useTranslatedPath(lang: keyof typeof ui) {
-  return function translatePath(path: string, l: string = lang) {
-    return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`
-  }
+  return function translatePath(path: string, l: keyof typeof ui = lang) {
+    if (!path.includes("/developers") && l !== defaultLang) {
+      return `/${l}${path}`;
+    }
+    // the translated path is /developers/es/blog, not /es/developers/blog
+    if (l !== defaultLang) {
+      const pathSegments = path.split("/");
+      pathSegments.splice(2, 0, l);
+      const translatedPath = pathSegments.join("/");
+      return translatedPath;
+    }
+    return path;
+  };
+}
+
+export function getEnglishPathFromUrl(url: URL) {
+  const pathSegments = url.pathname.split("/");
+  const filteredSegments = pathSegments.filter((segment) => segment !== "es");
+  const englishPath = filteredSegments.join("/");
+  return englishPath;
 }
