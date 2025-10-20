ec2f239258fb8c341d897e56cef3a01fe1c6a59e
JxM <mattheyj@gmail.com>
2025-10-20 19:14:55 +0200

    feat: add GitHub API integration for feedback widget

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   site: "https://interledger.org",
   base: "/developers",
   i18n: {
