50b5d8b2ef27b797378121f42438dd40915221ea
JxM <mattheyj@gmail.com>
2025-10-20 13:19:20 +0200

    fix: astro social icons config

--- astro.config.mjs
+++ astro.config.mjs
@@ -70,7 +70,7 @@ export default defineConfig({
           icon: "github",
           label: "GitHub",
           href: "https://github.com/interledger"
-        }
+        },
       ],
       sidebar: [
         {
