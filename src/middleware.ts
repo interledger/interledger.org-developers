7da2da277030fde664bb1abe7e28a4228f6dd08d
Jonathan Matthey <mattheyj@gmail.com>
2025-12-08 22:07:39 +0100

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,5 @@
-export default [
-  'strapi::errors',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['http://localhost:1103'],
-    },
-  },
-  'strapi::security',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+import type { MiddlewareHandler } from 'astro'
+
+export const onRequest: MiddlewareHandler = async (_, next) => {
+  return next()
+}
