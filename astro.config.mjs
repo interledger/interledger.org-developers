a89755eee02d59389b9d6d538d3eb5838bb2ab92
Infi-Knight <soni.ravi829@gmail.com>
2026-04-21 15:59:08 +0530

    chore(deps): upgrade dependencies for Astro 6 (DOCSDEV-60)

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,10 @@ export default defineConfig({
         starlightLinksValidator({
           exclude: ['/participation-guidelines']
         }),
-        starlightFullViewMode({ leftSidebarEnabled: false })
+        starlightFullViewMode({
+          leftSidebarEnabled: true,
+          rightSidebarEnabled: true
+        })
       ],
       head: [
         {
