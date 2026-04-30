ff89c03dfe0aafa9ee4a0230aef1fb9dba00b57e
Stephan Butler <stephan@interledger.foundation>
2026-04-30 13:38:40 +0200

    style: linting fix

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -5,10 +5,10 @@ export default async () => {
   <head><meta charset="utf-8"><title>Hello</title></head>
   <body><h1>Hello, World!</h1></body>
 </html>`,
-    { headers: { 'content-type': 'text/html; charset=utf-8' } },
-  );
-};
+    { headers: { 'content-type': 'text/html; charset=utf-8' } }
+  )
+}
 
 export const config = {
-  path: '/developers/api/hello',
-};
+  path: '/developers/api/hello'
+}
