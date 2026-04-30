1781fde7192b3e70fb762575835cd671e00043b7
Stephan Butler <stephan@interledger.foundation>
2026-04-30 12:59:07 +0200

    feat: poc ssr netlify rendering

---
+++ netlify/functions/hello.js
@@ -0,0 +1,14 @@
+export default async () => {
+  return new Response(
+    `<!doctype html>
+<html lang="en">
+  <head><meta charset="utf-8"><title>Hello</title></head>
+  <body><h1>Hello, World!</h1></body>
+</html>`,
+    { headers: { 'content-type': 'text/html; charset=utf-8' } },
+  );
+};
+
+export const config = {
+  path: '/developers/api/hello',
+};
